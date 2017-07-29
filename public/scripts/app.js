137ada0fb4bb2449f8ec4e0565610d1ed6c7f0b9
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-07-29 13:05:04 -1000

    trying to get button to work

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,2 +1,8 @@
 const test = document.getElementById('test');
+
 test.innerHTML = 'this works';
+
+
+var button = document.querySelector('#button');
+
+button.addEventListener('click', send.sendText);
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 /* jshint esversion:6*/
+
 const express = require('express');
 const PORT = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
@@ -11,24 +12,6 @@ const authToken = '34d56499ed938d2d2ea903fd3c647d60';
 var client = require('twilio')(accountSid, authToken);
 
 
-// client.messages.create({
-//   to: "+17209855392",
-//   from: "+18085183735",
-//   body: "this is a test"
-// }, function(err,message){
-//   if(err){
-//     console.log(err);
-//   }else{
-//     console.log(message.sid);
-//   }
-// })
-app.post('/sms', function(req,res){
-  var twilio = require('twilio');
-  var twiml = newtwilio.TwimResponse();
-  twiml.message('Testing this shit out!');
-  res.writehesad(200, {'Content-type': 'text/xml'});
-  res.end(twiml.toString());
-})
 app.listen(PORT, () => {
  console.log('hi ya nerd!');
 });
